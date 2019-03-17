af5d161c0b06f99975a1bbda9c606a07988c8e14
DeuklyoungKo <deutschgon@gmail.com>
2019-03-17 09:05:41 +0100

    install phpunit bundle

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20190316074700;
+$config['migration_version'] = 20190316084700;
 
 /*
 |--------------------------------------------------------------------------
--- application/libraries/Newsfixture.php
+++ application/libraries/Newsfixture.php
@@ -1,10 +1,46 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Newsfixture {
+class newsfixture {
 
-	public function show_hello_world()
+	protected $CI;
+
+	public function __construct()
 	{
-		echo 'Ok!!';
+		$this->CI =& get_instance();
+
+		$this->CI->load->database();
+		$this->CI->load->library('session');
 	}
+
+	public function add_newsFixtureData(Int $count = 20)
+	{
+
+		$this->CI->db->truncate('news');
+		//add flash data
+		$this->CI->session->set_flashdata('addnews',"truncated news table");
+
+		$this->CI->session->set_flashdata('addnews',"start add into news");
+		for ($i = 1; $i <= $count; $i++) {
+			$this->addnewsData();
+		}
+
+//		echo "Fixture completed".PHP_EOL;
+		$this->CI->session->set_flashdata('addnews',"Fixture completed");
+
+	}
+
+	public function addnewsData()
+	{
+
+		$object = array(
+			'title' => 'test1',
+			'slug' => 'test1_1123_123123',
+			'text' => 'hohohohoho'
+		);
+
+		$this->CI->db->insert('news', $object);
+
+	}
+
 }
